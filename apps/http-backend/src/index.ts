9b48fc58a6acf4eb2115e88cc679fef3ea0ffdfb
Asmit_Shukl <asmitshukla9794@gmail.com>
2026-02-22 16:53:16 +0530

    fix the the dotenv wala problem still not resolved

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,12 +2,27 @@ import  Express  from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import {CreateRoomSchema, CreateUserSchema, SigninSchema} from "@repo/common/types"
+import {client} from "@repo/db/client";
 
 const app=Express();
 app.use(Express.json());
  
 app.post("/signup",async(req,res)=>{
-    const data=CreateUserSchema.safeParse(req.body);
+    // const data=CreateUserSchema.safeParse(req.body);
+    const username=req.body.username;
+    const password=req.body.password;
+    const name=req.body.name;
+    const email=req.body.email;
+    await client.user.create({
+        data:{
+            email,
+            password,
+            name:username,
+        }
+    })
+    return res.json({
+        message:"u are done"
+    })
 })
 
 
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,22 @@
-import { PrismaClient } from "@prisma/client";
+//fix the dotenv wala problem it is not able to be exported from the dotenv 
+//waise export hoja raha hai magr dotenv se nahi ho pa rah hai woh fix karna hai
 
-export const client =new PrismaClient();
+import 'dotenv/config';
+
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';  
+
+const connectionString = 'postgresql://neondb_owner:npg_HYmIVeZ8Nhd2@ep-lively-block-aiyv5t0d-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require';
+
+const datasource={
+    url: process.env["DATABASE_URL"],
+  }
+
+console.log(datasource.url + " as");
+
+const pool = new Pool({ connectionString });
+
+const adapter = new PrismaPg(pool);  
+
+export const client = new PrismaClient({ adapter });
