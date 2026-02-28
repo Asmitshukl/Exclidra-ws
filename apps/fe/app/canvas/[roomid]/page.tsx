658215b5c8d8ee6bd28b594d3117aa459fcaa4f2
Asmit_Shukl <asmitshukla9794@gmail.com>
2026-03-01 00:26:19 +0530

    added rectangle

--- apps/fe/app/canvas/[roomid]/page.tsx
+++ apps/fe/app/canvas/[roomid]/page.tsx
@@ -1,5 +1,19 @@
-export default function canvas(){
-    <div>
-        drawing logic
-    </div>
+"use client"
+import Rect from "@/objects/rect";
+import { useEffect, useRef } from "react"
+
+export default function Canvas(){
+    const canvasref=useRef<HTMLCanvasElement>(null);
+
+    useEffect(()=>{
+        if(canvasref.current){
+            Rect(canvasref.current);
+        }
+    },[canvasref])
+
+    return (
+        <div>
+            <canvas ref={canvasref} width={1080}  height={1080} ></canvas>
+        </div>
+    )
 }
