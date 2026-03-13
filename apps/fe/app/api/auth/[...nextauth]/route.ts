762f4ed52471a727e2a3a507205a1b4ca448f917
Asmit_Shukl <asmitshukla9794@gmail.com>
2026-03-13 16:24:35 +0530

    started nextauth

--- apps/fe/app/api/auth/[...nextauth]/route.ts
+++ apps/fe/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,21 @@
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: "/signin",   // your existing login page
+    signOut:"/"
+  },
+  session: {
+    strategy: 'jwt',
+    maxAge: 1 * 24 * 60 * 60,
+  },
+});
+
+export { handler as GET, handler as POST };
